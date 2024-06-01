5f944bd5b7458ac64a28ec292052f9b68ebe0bf3
Yuri <yuri.araujo-ext@tuibrasil.com>
2024-06-01 16:40:05 -0300

    chore: add prettier and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: "src",
+  srcDir: 'src',
   devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
 });
