8c9d8ed94f914e5a208abb534796212b47ca4b13
Yuri Araújo <yuri.araujo@tui.co.uk>
2024-07-19 15:04:31 -0300

    chore: add Roboto font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/stylelint-module', '@pinia/nuxt'],
   css: ['~/assets/scss/main.scss'],
+  app: {
+    head: {
+      title: 'Trendy Recipes',
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap',
+        },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       apiBaseURL: process.env.API_BASE_URL,
