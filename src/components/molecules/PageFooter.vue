763d26d381f65900c63c248f4549472722cdad53
Yuri Araújo <yuri.araujo@tui.co.uk>
2024-07-21 18:16:55 -0300

    feat: add page footer

---
+++ src/components/molecules/PageFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <hr />
+    <p>Trendy Recipes</p>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  font-size: 0.85rem;
+  padding: 1.5rem 1rem;
+  text-align: center;
+}
+</style>
